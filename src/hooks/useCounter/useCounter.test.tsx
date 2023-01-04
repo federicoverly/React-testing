078705a275f2fdfb585c706cae65ac49ba4f79d3
Federico Verly <federicoverly@gmail.com>
2023-01-04 16:05:36 +0100

    Act utility

--- src/hooks/useCounter/useCounter.test.tsx
+++ src/hooks/useCounter/useCounter.test.tsx
@@ -1,5 +1,5 @@
 import { useCounter } from "./useCounter";
-import { renderHook } from "@testing-library/react";
+import { renderHook, act } from "@testing-library/react";
 
 test("Should render the initial count", () => {
   const { result } = renderHook(useCounter);
@@ -11,4 +11,25 @@ test("Should accept and render initial count", () => {
   const { result } = renderHook(() => useCounter({ initialCount: 10 }));
 
   expect(result.current.count).toBe(10);
+});
+
+// Act to mock an implementation
+test("Should increment the count", () => {
+  const { result } = renderHook(useCounter);
+
+  act(() => {
+    result.current.increment();
+  });
+
+  expect(result.current.count).toBe(1);
+});
+
+test("Should decrement the count", () => {
+  const { result } = renderHook(useCounter);
+
+  act(() => {
+    result.current.decrement();
+  });
+
+  expect(result.current.count).toBe(-1);
 });
